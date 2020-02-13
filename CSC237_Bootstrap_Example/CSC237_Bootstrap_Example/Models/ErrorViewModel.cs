8ae8ce5429b8070036493dd28340fa101f864b70
Nathaniel Brassell <nathaniel303@gmail.com>
2020-02-12 19:52:10 -0700

    Add files via upload

---
+++ CSC237_Bootstrap_Example/CSC237_Bootstrap_Example/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CSC237_Bootstrap_Example.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
